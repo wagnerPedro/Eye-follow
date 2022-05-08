ecd617dc8556ea662ddf2efa462d1741385161b9
Wagner shy <Wagnerpedro355@gmail.com>
2022-05-08 14:48:26 +0100

    concertei o bug na linha 10 já deve funcionar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const eye = document.querySelector('.iris');
 
 window.addEventListener('mousemove', (e) => {
+    
     const x = -(window.innerWidth / 2 - e.pageX) / 35;
 
     const y = -(window.innerHeight / 2 - e.pageY) / 35;
 
     eye.style.transform = `rotate(-45deg) 
-    translateY(${y}px translate(${x}px))`
+    translateY(${y}px) translateX(${x}px)`;
 });
